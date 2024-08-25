16e8090935d0eff0e0411e4b471b05336e1a4f39
Avik-creator <avikm744@gmail.com>
2024-08-25 22:12:01 +0530

    Initial Commit

---
+++ Quiz-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quiz-App
+//
+//  Created by AVIK MUKHERJEE on 25/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quiz-App/Quiz_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_AppApp.swift
+//  Quiz-App
+//
+//  Created by AVIK MUKHERJEE on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
